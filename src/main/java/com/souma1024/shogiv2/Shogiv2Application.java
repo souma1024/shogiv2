c16654d8b37363c882b36806c1c43ef1261deef7
souma <souma3789522@gmail.com>
2025-06-27 19:24:39 +0900

    first commit

---
+++ src/main/java/com/souma1024/shogiv2/Shogiv2Application.java
@@ -0,0 +1,13 @@
+package com.souma1024.shogiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shogiv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shogiv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/souma1024/shogiv2/Shogiv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souma1024.shogiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shogiv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
